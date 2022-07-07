7c88f41fe910ce90cbc0a06c1cc80485b84b7d2c
Sebastian Bode <sebastian.bode@cardanofoundation.org>
2022-07-07 21:58:51 +0200

    Refactoring of folder structure

--- database/scripts/post_migration.sql
+++
@@ -1,1 +0,0 @@
-GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE ON ALL TABLES IN SCHEMA "public" TO :"cf_metadata_serviceuser_name";
--- database/scripts/teardown.sql
+++
@@ -1,6 +0,0 @@
-REVOKE CONNECT ON DATABASE :"cf_metadata_dbname" FROM :"cf_metadata_serviceuser_name";
-SELECT pid, pg_terminate_backend(pid) 
-FROM pg_stat_activity 
-WHERE datname = :'cf_metadata_dbname' AND pid <> pg_backend_pid();
-DROP DATABASE IF EXISTS :"cf_metadata_dbname";
-DROP USER IF EXISTS :"cf_metadata_serviceuser_name";
