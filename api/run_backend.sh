57549b6a60daf1261e4cdb5d498444a364534c84
Thomas Nguyen <thien.nguyen3@sotatek.com>
2022-09-19 12:01:55 +0700

    add printenv to debug

--- api/run_backend.sh
+++ api/run_backend.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+printenv
+
+echo AWS_SSH
+echo $DB_CONNECTION_PARAMS_PROVIDER_TYPE
+echo $REGION
+echo $RDS_USERNAME_SSM_PARAMETER_NAME
+echo $RDS_PASSWORD_SSM_PARAMETER_NAME
+echo $RDS_URL_SSM_PARAMETER_NAME
+
+echo NOT AWS_SSH
+echo $DB_URL
+echo $DB_USER
+echo $DB_SECRET
+echo $DB_DRIVER_CLASS_NAME
+
 if [ "${DB_CONNECTION_PARAMS_PROVIDER_TYPE}" = "AWS_SSM" ]
 then
   exec java -jar -DdbConnectionParamsProviderType="${DB_CONNECTION_PARAMS_PROVIDER_TYPE}" -Dregion="${REGION}" -DrdsUsernameSsmParameterName="${RDS_USERNAME_SSM_PARAMETER_NAME}" -DrdsPasswordSsmParameterName="${RDS_PASSWORD_SSM_PARAMETER_NAME}" -DrdsUrlSsmParameterName="${RDS_URL_SSM_PARAMETER_NAME}" -DrdsDriverClassNameSsmParameterName="${RDS_DRIVER_CLASS_NAME_SSM_PARAMETER_NAME}" ./app.jar
