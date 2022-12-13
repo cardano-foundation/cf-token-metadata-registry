f23f5747c232b30a42da13638b5c99f7f2b94957
Sebastian Bode <sebastian.bode@cardanofoundation.org>
2022-12-13 10:46:44 +0100

    Removed AWS dependencies from code and adapted command to run locally

--- api/src/main/java/org/cardanofoundation/metadatatools/api/config/DatasourceConfig.java
+++ api/src/main/java/org/cardanofoundation/metadatatools/api/config/DatasourceConfig.java
@@ -4,24 +4,14 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.ssm.SsmClient;
-import software.amazon.awssdk.services.ssm.model.GetParameterRequest;
-import software.amazon.awssdk.services.ssm.model.GetParameterResponse;
-import software.amazon.awssdk.services.ssm.model.SsmException;
 
 import javax.sql.DataSource;
 
 @Log4j2
 @Configuration
 public class DatasourceConfig {
-    private enum DatabaseConnectionParametersProviderType {
-        ENVIRONMENT, AWS_SSM
-    }
-
     private static final String DEFAULT_DB_DRIVER_NAME = "org.postgresql.Driver";
     private static final String DEFAULT_DB_URL = "jdbc:postgresql://localhost:5432/cf_metadata";
-    private static final String DEFAULT_DB_CONNECTION_PARAMS_PROVIDER_TYPE = DatabaseConnectionParametersProviderType.ENVIRONMENT.name();
 
     private DatabaseConnectionParameters getConnectionParametersFromEnvironment() {
         final DatabaseConnectionParameters params = new DatabaseConnectionParameters();
@@ -32,43 +22,16 @@ public class DatasourceConfig {
         return params;
     }
 
-    private DatabaseConnectionParameters getConnectionParametersFromAwsSsm() {
-        final Region region = Region.of(System.getProperty("region"));
-        final DatabaseConnectionParameters params = new DatabaseConnectionParameters();
-        params.setUsername(getEncryptedSsmParameter(System.getProperty("rdsUsernameSsmParameterName"), region));
-        params.setPassword(getEncryptedSsmParameter(System.getProperty("rdsPasswordSsmParameterName"), region));
-        params.setUrl(getEncryptedSsmParameter(System.getProperty("rdsUrlSsmParameterName"), region));
-        params.setDriverClassName(getEncryptedSsmParameter(System.getProperty("rdsDriverClassNameSsmParameterName"), region));
-        return params;
-    }
-
     @Bean
     public DataSource getDataSource() {
-        final DatabaseConnectionParametersProviderType secretsProviderType = DatabaseConnectionParametersProviderType.valueOf(System.getProperty("dbConnectionParamsProviderType", DEFAULT_DB_CONNECTION_PARAMS_PROVIDER_TYPE));
-        final DatabaseConnectionParameters databaseConnectionParams = switch (secretsProviderType) {
-            case ENVIRONMENT -> getConnectionParametersFromEnvironment();
-            case AWS_SSM -> getConnectionParametersFromAwsSsm();
-        };
-
+        final DatabaseConnectionParameters databaseConnectionParams = getConnectionParametersFromEnvironment();
         final DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.driverClassName(databaseConnectionParams.getDriverClassName());
         dataSourceBuilder.url(databaseConnectionParams.getUrl());
         dataSourceBuilder.username(databaseConnectionParams.getUsername());
         dataSourceBuilder.password(databaseConnectionParams.getPassword());
+        log.info(String.format("Trying to connect to database %s with driver %s",
+                databaseConnectionParams.getUrl(), databaseConnectionParams.getDriverClassName()));
         return dataSourceBuilder.build();
-    }
-
-    private static String getEncryptedSsmParameter(final String parameterName, final Region region) {
-        try (SsmClient ssmClient = SsmClient.builder().region(region).build()) {
-            final GetParameterRequest parameterRequest = GetParameterRequest.builder()
-                    .name(parameterName)
-                    .withDecryption(true)
-                    .build();
-            final GetParameterResponse parameterResponse = ssmClient.getParameter(parameterRequest);
-            return parameterResponse.parameter().value();
-        } catch (final SsmException e) {
-            log.error("Could not fetch database connection parameter information from AWS SSM.", e);
-            return "";
-        }
     }
 }
