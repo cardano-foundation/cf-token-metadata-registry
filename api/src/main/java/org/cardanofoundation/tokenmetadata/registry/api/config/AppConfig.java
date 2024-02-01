03f15b034eda88e5c56a75267ca0bb3bf16f4a11
Giovanni Gargiulo <giovanni.gargiulo@cardanofoundation.org>
2024-02-01 15:40:30 +0000

    chore: enabled scheduling

---
+++ api/src/main/java/org/cardanofoundation/tokenmetadata/registry/api/config/AppConfig.java
@@ -0,0 +1,9 @@
+package org.cardanofoundation.tokenmetadata.registry.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class AppConfig {
+}
