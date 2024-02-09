a53b761a408eea7bc49da02a1f420dcdd42f785c
Giovanni Gargiulo <giovanni.gargiulo@cardanofoundation.org>
2024-02-09 13:59:04 +0000

    fix: fixed spring configuration

--- api/src/main/java/org/cardanofoundation/tokenmetadata/registry/api/TokenMetadataRegistryApplication.java
+++ api/src/main/java/org/cardanofoundation/tokenmetadata/registry/api/TokenMetadataRegistryApplication.java
@@ -3,14 +3,18 @@ package org.cardanofoundation.tokenmetadata.registry.api;
 import jakarta.servlet.DispatcherType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 
 import java.util.List;
 
 @SpringBootApplication(scanBasePackages = "org.cardanofoundation.tokenmetadata.registry")
+@EnableJpaRepositories("org.cardanofoundation.tokenmetadata.registry.repository")
+@EntityScan(basePackages = "org.cardanofoundation.tokenmetadata.registry.entity")
 public class TokenMetadataRegistryApplication {
     public static void main(String[] args) {
         SpringApplication.run(TokenMetadataRegistryApplication.class, args);
