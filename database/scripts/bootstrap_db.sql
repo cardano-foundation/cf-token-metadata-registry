f54c6f43912ac01f32bc20703a10d6f631232829
Mateusz Czeladka <mateusz.czeladka@cardanofoundation.org>
2025-09-11 15:18:19 +0200

    fix: db fixes

--- database/scripts/bootstrap_db.sql
+++ database/scripts/bootstrap_db.sql
@@ -1,2 +1,4 @@
-CREATE USER cardano PASSWORD 'metadata1337_';
-CREATE DATABASE cf_token_metadata_registry WITH OWNER cardano;
+-- The database is created automatically by POSTGRES_DB env variable
+-- Only create user if it doesn't exist (postgres will be the default user)
+-- Grant all privileges on the database to the user
+GRANT ALL PRIVILEGES ON DATABASE cf_token_metadata_registry TO cardano;
